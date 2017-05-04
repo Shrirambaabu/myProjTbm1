a3a56c41613367a9964663030833e31476bb3699
ashithvl <ashithvl@gmail.com>
2017-05-04 21:08:39 +0530

    registration Ui changes

--- app/src/main/java/blueangels/com/layouts/RegisterActivity.java
+++ app/src/main/java/blueangels/com/layouts/RegisterActivity.java
@@ -1,22 +1,15 @@
 package blueangels.com.layouts;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
-import static blueangels.com.layouts.R.id.parent;
 
 /**
  * Created by Admin on 5/2/2017.
@@ -24,19 +17,32 @@ import static blueangels.com.layouts.R.id.parent;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private String[] passOutYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getSupportActionBar().hide();
-
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
         setContentView(R.layout.activity_registration);
+
+        String[] passOutYear = getResources().getStringArray(R.array.year_arrays);
+        List<String> plantsList = new ArrayList<String>();
+
+        Collections.addAll(plantsList, passOutYear);
+
+        Spinner spinner = (Spinner) findViewById(R.id.passed_out_year_spinner);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item_custom, plantsList);
+        spinner.setPrompt("  -- Select the Passout Year -- ");
+        spinner.setAdapter(spinnerArrayAdapter);
+
     }
 
     public void registerNew(View view) {
 
-        Intent registerationCompleteIntent = new Intent(RegisterActivity.this, RegisterPasswordActivity.class);
-        startActivity(registerationCompleteIntent);
+        Intent registrationCompleteIntent = new Intent(RegisterActivity.this, RegisterPasswordActivity.class);
+        startActivity(registrationCompleteIntent);
 
     }
 }
