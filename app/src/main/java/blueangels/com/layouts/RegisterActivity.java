6202c8e8c473ec0b3c3f61a82f740ab87d3909bb
Shrirambaabu <shrirambaabu0902@gmail.com>
2017-05-04 15:07:15 +0530

    Spinner check

--- app/src/main/java/blueangels/com/layouts/RegisterActivity.java
+++ app/src/main/java/blueangels/com/layouts/RegisterActivity.java
@@ -1,9 +1,22 @@
 package blueangels.com.layouts;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static blueangels.com.layouts.R.id.parent;
 
 /**
  * Created by Admin on 5/2/2017.
@@ -22,7 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void registerNew(View view) {
 
-        Intent registerationCompleteIntent = new Intent(RegisterActivity.this,RegisterPasswordActivity.class);
+        Intent registerationCompleteIntent = new Intent(RegisterActivity.this, RegisterPasswordActivity.class);
         startActivity(registerationCompleteIntent);
 
     }
