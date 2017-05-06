210d8a709231a77f1ef70ee0f92d86dd005f6251
Shrirambaabu <shrirambaabu0902@gmail.com>
2017-05-06 13:41:05 +0530

    Passout

--- app/src/main/java/blueangels/com/layouts/SplashScreenActivity.java
+++ app/src/main/java/blueangels/com/layouts/SplashScreenActivity.java
@@ -16,7 +16,10 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+/**
+ * // If your minSdkVersion is 11 or higher, instead use:
+ //getActionBar().hide();
+ **/
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_splash_screen);
@@ -26,12 +29,14 @@ public class SplashScreenActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
+                /** // This method will be executed once the timer is over
+                 // Start your app main activity
+                 **/
                 Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(i);
-
+                /*
                 // close this activity
+                */
                 finish();
             }
         }, SPLASH_TIME_OUT);
