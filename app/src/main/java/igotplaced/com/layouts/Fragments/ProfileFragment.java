b0e6597fd0bc43c804c7085636f22eb55056e9f2
ashithvl <ashithvl@gmail.com>
2017-05-24 16:00:21 +0530

    push

--- app/src/main/java/igotplaced/com/layouts/Fragments/ProfileFragment.java
+++ app/src/main/java/igotplaced/com/layouts/Fragments/ProfileFragment.java
@@ -1,27 +1,111 @@
 package igotplaced.com.layouts.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import igotplaced.com.layouts.R;
 
 
 public class ProfileFragment extends Fragment {
 
+
+    private Context context;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_mail_outline_white_36dp,
+            R.drawable.ic_mail_outline_white_36dp,
+            R.drawable.ic_mail_outline_white_36dp,
+            R.drawable.ic_mail_outline_white_36dp
+    };
+
     public ProfileFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        context = getContext();
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+        setupViewPager(viewPager);
+        // Give the TabLayout the ViewPager
+        tabLayout = (TabLayout) view.findViewById(R.id.sliding_tabs);
+
+        setupTabIcons();
+
+
+        return view;
+
+    }
+
+    private void setupTabIcons() {
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+                for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                    TabLayout.Tab tab = tabLayout.getTabAt(i);
+                    if (tab != null) {
+                        tab.setIcon(tabIcons[i]);
+                    }
+                }
+            }
+        });
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new ProfilePostFragment());
+        adapter.addFragment(new ProfileInterviewQuestionFragment());
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            //return mFragmentTitleList.get(position);
+            return null;
+        }
     }
 
 }
